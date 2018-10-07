a6e04653d337e76fa0c411de5fbcd81ebcaa78d5
MichelleCharotti <michellecharotti@gmail.com>
2018-10-06 22:58:19 -0300

    Tp2

---
+++ TP-02-Charotti.Michelle.2A/Entidades/Snacks.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_2018
+{
+    public class Snacks:Producto
+    {
+        /// <summary>
+        /// Los snacks tienen 104 calorías
+        /// </summary>
+        protected override short CantidadCalorias
+        {
+            get
+            {
+                return 104;
+            }
+        }
+
+        public Snacks(EMarca marca, string patente, ConsoleColor color): base(patente, marca, color)
+        {
+        }
+
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("SNACKS");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendFormat("CALORIAS : {0}\n", this.CantidadCalorias.ToString());
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
